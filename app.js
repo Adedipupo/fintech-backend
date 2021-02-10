b2971b0465b1f05722926e9e4468b30764da8b7c
unknown <mugambijames96@gmail.com>
2021-02-10 23:52:25 +0300

    add coop bank validate account, check balance

--- app.js
+++ app.js
@@ -50,11 +50,11 @@ app.use("/api", coopBankRoutes);
 app.use("/api", chargeRoutes);
 app.use("/api", serviceRoutes);
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 app.listen(process.env.PORT, "0.0.0.0");
 
